1c76565ae094ad0c37bd499a1af3140db2fef732
kirluc11 <kirluc11@htlkaindorf.at>
2014-10-16 15:01:02 +0200

    Dere commit

---
+++ src/selector/QuestionSelector.java
@@ -0,0 +1,20 @@
+package selector;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+
+/**
+ * Created by Lukas on 16.10.2014.
+ */
+public class QuestionSelector implements Serializable{
+
+
+
+    public QuestionSelector() {
+        laodData();
+    }
+
+    private void laodData() {
+
+    }
+}
