e32e23d665557db117f6424f7be852d08a11bef8
segalc11 <segalc11@htlkaindorf.at>
2014-11-06 14:41:42 +0100

    Did stuff

--- src/com/example/Project_Marvin/MyActivity.java
+++ src/com/example/Project_Marvin/MyActivity.java
@@ -37,7 +37,12 @@ public class MyActivity extends Activity implements View.OnClickListener
             System.out.println("Error " + e.getMessage());
         }
 
-        setContentView(R.layout.main);
+        if(getIntent().getIntExtra("screenNumber",0)==0)setContentView(R.layout.main);
+        else if(getIntent().getIntExtra("screenNumber",0)==1)setContentView(R.layout.general_playscreen);
+        else if(getIntent().getIntExtra("screenNumber",0)==2)setContentView(R.layout.info_screen);
+        else setContentView(R.layout.credits_screen);
+
+
         deu=(Button) findViewById(R.id.deu);
         eng=(Button) findViewById(R.id.eng);
         credits =(Button) findViewById(R.id.credits);
@@ -45,14 +50,52 @@ public class MyActivity extends Activity implements View.OnClickListener
         info=(Button) findViewById(R.id.info);
         info.setOnClickListener(this);
         start=(Button) findViewById(R.id.start);
+        start.setOnClickListener(this);
     }
 
 
-    @Override
+
+    int getScreenNumber()
+    {
+        return getIntent().getIntExtra("screenNumber",0);
+    }
+
+    void goToNextScreen (int whichButton)
+    {
+        int screenNumber = getScreenNumber();
+
+        //whichButton = 1 -> Start
+        //whichButton = 2 -> Info
+        //whichButton = 3 -> Credits
+
+        final Intent nextIntent=new Intent(this, MyActivity.class);
+        if(whichButton==1)
+        {
+            nextIntent.putExtra("screenNumber",1);
+        }
+        else if(whichButton==2)
+        {
+            nextIntent.putExtra("screenNumber",2);
+        }
+        else
+        {
+            nextIntent.putExtra("screenNumber",3);
+        }
+
+        System.out.printf("goToNextScreen: intExtra=%d\n", screenNumber);
+
+        startActivity(nextIntent);
+
+        /*
+        Attention: startActivity calls the onCreate method if it contains a main.xml, this will be rendered.
+        Add code in onCreate to modify the main.xml if desired.
+         */
+    }
+
+
     public void onClick(View v)
     {
-        final Intent i= getIntent();
-        final Intent nextIntent=new Intent(this, MyActivity.class);
-        startActivity(nextIntent);
+        Button button=(Button) v;
+        System.out.println(button.getTag()+"");
     }
 }
